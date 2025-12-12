76afa1df48d44e2e245618dd93fa8990e55f1b67
teja pendem <pendem.teja08@gmail.com>
2025-12-12 10:49:39 +0000

    first commit

---
+++ webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("com.demo.northwindproducts.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
